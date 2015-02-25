f85b7930772d235330b492d047165dbff6939eab
Peter Grace <pete@stackoverflow.com>
2015-02-24 22:12:03 -0500

    Fix formatting in script so it is readable from github page

--- maint.sh
+++ maint.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-#This script will turn on maintenance mode for an elastic cluster.  When `maint.sh 1` is issued, elastic will not attempt to heal missing shards.
+#This script will turn on maintenance mode for an elastic cluster.  When `maint.sh 1` is issued, elastic will not attempt to heal
+#missing shards.
 #
-#You would use this script in an upgrade scenario or when you are anticipating removing a node for a specific purpose for a short period of time.
+#You would use this script in an upgrade scenario or when you are anticipating removing a node for a specific purpose for a short
+#period of time.
 #
-#`maint.sh 0` will turn off this maintenance mode and make the shards come back online.  Only execute this after the node is back online and connected to the cluster.
+#`maint.sh 0` will turn off this maintenance mode and make the shards come back online.  Only execute this after the node is back
+#online and connected to the cluster.
 #
+
 if [ -z $1 ]
 then
         echo "0 or 1"
